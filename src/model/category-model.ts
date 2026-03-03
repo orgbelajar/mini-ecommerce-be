0f13edbcf0396bec9bc790dc6ef4c2b316df8afb
nabbiil._ <nabilsyakir95@gmail.com>
2026-03-03 17:12:14 +0700

    feat(category): tambah slug pada response kategori

--- src/model/category-model.ts
+++ src/model/category-model.ts
@@ -8,6 +8,7 @@ export type CategoryRequest = {
 export type CategoryResponse = {
   categoryId: string;
   name: string;
+  slug?: string;
   createdAt?: Date;
   updatedAt?: Date;
 };
@@ -15,6 +16,7 @@ export type CategoryResponse = {
 export type CategoryWithProductsResponse = {
   categoryId: string;
   name: string;
+  slug?: string;
   createdAt: Date;
   updatedAt: Date;
   products: ProductResponse[];
@@ -24,6 +26,7 @@ export const toAddCategoryResponse = (category: Category): CategoryResponse => {
   return {
     categoryId: category.id,
     name: category.name,
+    slug: category.slug,
     createdAt: category.createdAt,
   };
 };
@@ -34,6 +37,7 @@ export const toGetCategoryByIdResponse = (
   return {
     categoryId: category.id,
     name: category.name,
+    slug: category.slug,
     createdAt: category.createdAt,
     updatedAt: category.updatedAt,
   };
@@ -55,6 +59,7 @@ export const toGetCategoryWithProductsResponse = (
   return {
     categoryId: category.id,
     name: category.name,
+    slug: category.slug,
     createdAt: category.createdAt,
     updatedAt: category.updatedAt,
     products: category.products.map(toProductResponse),
